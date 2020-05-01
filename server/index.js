31e755a5696a8ad30b727621f0f81d49cdf24043
devdrums <devdrums@devdrumss-MBP.fios-router.home>
2020-04-30 23:34:11 -0400

    feat: wired up with client and server

--- server/index.js
+++ server/index.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const morgan = require("morgan");
+const morgan = require("morgan");
 const path = require("path");
 
 const app = express();
 
-// app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "../client")));
+
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api", require("./api"));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "../public/index.html"));
+	res.sendFile(path.join(__dirname, "../client/index.html"));
 });
 
 app.use((err, req, res) => {
