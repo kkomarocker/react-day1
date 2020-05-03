5bdfdb8c78ee9616b14eb4794f896607ca0c9775
devdrums <devdrums@devdrumss-MBP.fios-router.home>
2020-05-03 08:51:41 -0400

    registering user

--- server/api/user.js
+++ server/api/user.js
@@ -1,10 +1,50 @@
 const router = require("express").Router();
 const User = require("../db/user");
+const bcrypt = require("bcrypt");
 
-
-router.get("/register", (req, res, next) => {
+router.get("/users", async (req, res, next) => {
 	try {
-		res.send("register user");
+		const data = await User.findAll();
+		res.send(data);
+	} catch (err) {
+		next(err);
+	}
+});
+
+router.post("/register", async (req, res, next) => {
+	try {
+		const { userName, firstName, lastName, email, password } = req.body;
+		const saltRound = 10;
+		let hashedPassword = "";
+
+		await bcrypt.hash(password, saltRound, (err, hash) => {
+			if (err) console.log(err);
+			hashedPassword = hash;
+
+			User.findOrCreate({
+				where: {
+					userName,
+				},
+				defaults: {
+					userName,
+					firstName,
+					lastName,
+					email,
+					password: hashedPassword,
+				},
+			}).then(([user, created]) => {
+				const createdUser = user.get({ plain: true });
+				const { userName, createdAt } = createdUser;
+
+				const data = {
+					userName,
+					createdAt,
+					created,
+				};
+
+				res.send(data);
+			});
+		});
 	} catch (err) {
 		next(err);
 	}
